6fed3f2a00c6e970cdd84cc370263c64d479cf37
Hack Reactor Student <student@hackreactor.com>
2018-08-09 15:37:17 -0500

    Needs recursion

--- src/solvers.js
+++ src/solvers.js
@@ -12,30 +12,63 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
+const zeroRowCol = (row, col, n, matrix)=>{
+  for(let i = 0; i < n; i++){
+    matrix[row][i] = 0;
+    matrix[i][col] = 0;
+  }
+};
 
+const zeroMajorDiags = (row, col, n, matrix)=>{
+  // move to left or top of matrix & calc length
+  let magicNumber;
+  if (row <= col) {
+    col -= row;
+    row -= row;
+    magicNumber = n - col;
+  } else if (row > col) {
+    row -= col;
+    col -= col;
+    magicNumber = n - row;
+  }
+  // add zeros
+  for(let i = 0; i < magicNumber; i++){
+    matrix[row][col] = 0;
+    row = (row + 1);
+    col = (col + 1);
+  }
+};
 
+const zeroMinorDiags = function(row,col,n,matrix) {
+  // move to top or right of matrix
+  let magicNumber;
+  let tempNum = row + col;
+  let tempNum2 = tempNum - (n - 1);
+  row = tempNum2;
+  col = n - 1;
+  if(row < 0){
+    col += row;
+    row -= row;
+  }
+  // calc length
+  magicNumber = col - row + 1;
+  // add zeros
+  for(let i = 0; i < magicNumber; i++) {
+    matrix[row][col] = 0; 
+    row = (row + 1) % n;
+    col = (col - 1);
+    if (col < 0) {
+      col = col + n;
+    }
+    
+  }
+}  
 
 window.findNRooksSolution = function(n) {
   let inputMatrix = [];
   for(let i = 0; i < n; i++){
     inputMatrix.push([]);
   }
-  // let amIOne = true;
-  // let counter = 0;
-  // for(let row = 0; row < n; row++){
-  //   let tempRow = [];
-  //   for(let col = 0; col < n; col++){
-  //     if(amIOne && i === counter){
-  //       tempRow.push(1);
-  //       amIOne = false;
-  //       counter++;
-  //     } else {
-  //       tempRow.push(0);
-  //     }
-  //   }
-  //   inputMatrix.push(tempRow);
-  //   amIOne = true;
-  // }
   for(let row = 0; row < n; row++){
     for(let col = 0; col < n; col++){
       if(inputMatrix[row][col] === undefined){
@@ -64,122 +97,143 @@ window.countNRooksSolutions = function(n) {
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n,coordPair) {
-  let inputMatrix = [];
-  for(let i = 0; i < n; i++){
-    inputMatrix.push([]);
+window.findNQueensSolution = function(n, coordArr, board, winningBoardSet) {
+  // let inputMatrix = [];
+  // for(let i = 0; i < n; i++){
+  //   inputMatrix.push([]);
+  // }
+  if(n === 0 ){
+    return [];
   }
-  let start = coordPair ? coordPair : [0,0]; // if provided with start cool, otherwise 0,0
   
-  //all loops (row, length, row0,col0,mjdiag0,mndiag0) must have modulo functionality
-  //declare queen counter as 0
-  //set row & col to coordPair or 0,0 if none provided
+  let solution = undefined;
   
-  //for row; vs length of n; row++ ----- do we have a problem here where we need to run four times 
-  //                                     but we might start our loop at row (from coordPair)? I 
-  //                                     you solved problem but I don't remember.                                        
-
-    // for col; vs length of n; col++ ----- see comment 4 lines up
-
-      // if coordPair = undefined
-
-        // increment number of queens
-        
-        // call row zeroing function
-          // loop over entire row (even if some points/cells are 'behind' you) using modulo
-          // if hit 1
-            // break current loop as producing invalid solution
-          // if hit 0 or undefined write them to 0
-        
-        // call column zeroing function
-          // loop over entire row (even if some points/cells are 'behind' you) using modulo
-          // if hit 1
-            // break current loop as producing invalid solution
-          // if hit 0 or undefined write them to 0
-        
-        // call major diagonal zeroing function
-          // loop over entire row (even if some points/cells are 'behind' you) using modulo
-          // if hit 1
-            // break current loop as producing invalid solution
-          // if hit 0 or undefined write them to 0
-        
-        // call minor diagonal zeroing function
-          // loop over entire row (even if some points/cells are 'behind' you) using modulo
-          // if hit 1
-            // break current loop as producing invalid solution
-          // if hit 0 or undefined write them to 0
-        
-        // set coordPair from 0 (because of zeroing functions) to 1 (queen)
-      
-      //end of 'if coordPair = undefined'
-    
-    //end of 'for col loop'
-  
-  //end of 'for row loop'
-  
-  //if queen counter is >= n
-    // ideally (for countNQueensSolutions) also save/return the starting coordinates that resulted
-      // in this solution so you can start on them + 1 for finding next solution
-    // return board as a valid solution for board of n
-  //else
-    // return false (no viable solutions for board of n)
-  
-  
-  
-  
-  var solution = undefined; //fixme
+  // places first queen
+  for(let m = 0; m < n; m++){
+    // if(solution !== undefined){
+    //   return solution;
+    // }
+    for(let p = 0; p < n; p++){
+      let qCounter = 0;
+      let newBoard = [];
+      // add rows to new board
+      for(let i = 0; i < n; i++){
+        newBoard.push([]);
+      }
+      //startpositionrow = m
+      //startpositioncol = p
+      let outerRow = m;
+      let outerCol = p;
+      for(let k = 0; k < n; k++){
+        for(let l = 0; l < n; l++){
+          let row = (outerRow + k) % n;
+          let col = (outerCol + l) % n;
+          let space = newBoard[row][col];
+          if(space === undefined){
+            zeroRowCol(row, col, n, newBoard);
+            zeroMajorDiags(row, col, n, newBoard);
+            zeroMinorDiags(row, col, n, newBoard);
+            newBoard[row][col] = 1;
+            qCounter++;
+          }
+        }
+      }
+      console.log(newBoard);
+      if(qCounter >= n){
+        solution = newBoard;
+        // break;
+      }
+    } // end of outer p loop
+  } // end of outer m loop
+       
+  //let start = coordPair ? coordPair : [0,0]; if provided with start cool, otherwise 0,0
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  if(solution === undefined){
+    let badBoard = [];
+    for(let z = 0; z < n; z++){
+      let tempRow = [];
+      for(let y = 0; y < n; y++){
+        tempRow.push(0)
+      }
+      badBoard.push(tempRow);
+    }
+    solution = badBoard;
+  }
+  return solution
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
   
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
-};
-
-  // var inputMatrix = [];
-  // for (var row = 0; row < n; row++) {
-  //   for (var col = 0; col < n; col++) {
-  //     inputMatrix = [];
-  //     for(let i = 0; i < n; i++){
-  //       inputMatrix.push([]);
-  //     }
-      
-  //   }
+  
+  // var solutionCount = 0;
+  // if(n === 0 ){
+  //   return [];}
+  // var winnerSet = new Set();
+  // if(n === 0 ){
+  //   return 1;
   // }
-
-var zeroMinorDiags = function(startX,startY,n) {
-      let inputMatrix = [['u','u','u','u'], // pretend 'u' is undefined - this just makes
-                         ['u','u','u','u'], // logs/output more readable. it works on undefined
-                         ['u','u','u','u'], // as well.
-                         ['u','u','u','u']];
-      let row = startX; // might be able to just use row or startX
-      let col = startY; // I couldn't remember if increment mutates
-                        // and if it mattered if it did in this case
-
-      for(let i = 0; i < n; i++) { 
-        inputMatrix[row][col] = 0;
-        row++;
-        col--;
-        if (row > n-1) { 
-          row = row % n;
-        }
-        if (col < 0) { // modulo doesn't seem to work well in this case
-                       // (for col hitting -1) or I'm misunderstanding the math
-                       // as far as I can tell this will have a similar effect
-          col = col + n;
+  
+  // if(n === 5){
+  //   return 10;
+  // }
+  
+  // if(n === 6){
+  //   return 4;
+  // }
+  
+  // if(n === 7){
+  //   return 40;
+  // }
+  
+  // let solution = undefined;
+  
+  // places first queen
+  for(let m = 0; m < n; m++){
+    // if(solution !== undefined){
+    //   return solution;
+    // }
+    for(let p = 0; p < n; p++){
+      let qCounter = 0;
+      let newBoard = [];
+      // add rows to new board
+      for(let i = 0; i < n; i++){
+        newBoard.push([]);
+      }
+      //startpositionrow = m
+      //startpositioncol = p
+      let outerRow = m;
+      let outerCol = p;
+      for(let k = 0; k < n; k++){
+        for(let l = 0; l < n; l++){
+          let row = (outerRow + k) % n;
+          let col = (outerCol + l) % n;
+          let space = newBoard[row][col];
+          if(space === undefined){
+            zeroRowCol(row, col, n, newBoard);
+            zeroMajorDiags(row, col, n, newBoard);
+            zeroMinorDiags(row, col, n, newBoard);
+            newBoard[row][col] = 1;
+            qCounter++;
+          }
         }
       }
-      // easily readable test output:
-      // console.log(inputMatrix[0]);
-      // console.log(inputMatrix[1]);
-      // console.log(inputMatrix[2]);
-      // console.log(inputMatrix[3]);
-}
+      console.log(newBoard);
+      console.log(qCounter);
+      if(qCounter >= n){
+        //stringify winning board
+        winnerSet.add(JSON.stringify(newBoard));
+        
+        //psuh to winner to set
+        //return set.length
+      
+        //solutionCount++;
+        // break;
+      }
+    } // end of outer p loop
+  } 
+  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  return winnerSet.size;//return solutionCount;
+};
 
-// test invocation:
-// hasMinorDiagonalConflictAt(1,3,4);
